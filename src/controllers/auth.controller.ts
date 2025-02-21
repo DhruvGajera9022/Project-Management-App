ec1074394cf3e079d44815aa6d2d2fcab787ed4f
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-02-21 16:03:41 +0530

    Add login and logout api

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { asyncHandler } from "../middlewares/asyncHandler.middleware";
 import { config } from "../config/app.config";
 import { registerSchema } from "../validation/auth.validation";
 import { HttpStatus } from "../config/http.config";
 import { registerUserService } from "../services/auth.service";
+import passport from "passport";
 
 export const googleLoginCallback = asyncHandler(
   async (req: Request, res: Response) => {
@@ -29,5 +30,66 @@ export const registerUserController = asyncHandler(
     return res.status(HttpStatus.CREATED).json({
       message: "User created successfully",
     });
+  }
+);
+
+export const loginController = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate(
+      "local",
+      (
+        err: Error | null,
+        user: Express.User | false,
+        info: { message: string } | undefined
+      ) => {
+        if (err) {
+          return next(err);
+        }
+
+        if (!user) {
+          return res.status(HttpStatus.UNAUTHORIZED).json({
+            message: info?.message || "Invalid email or password",
+          });
+        }
+
+        req.logIn(user, (err) => {
+          if (err) {
+            return next(err);
+          }
+
+          return res.status(HttpStatus.OK).json({
+            message: "Logged in successfully",
+            user,
+          });
+        });
+      }
+    )(req, res, next);
+  }
+);
+
+export const logOutController = asyncHandler(
+  async (req: Request, res: Response) => {
+    req.logout((err) => {
+      if (err) {
+        console.error("Logout error:", err);
+        return res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .json({ error: "Failed to log out" });
+      }
+    });
+
+    if (req.session) {
+      req.session.destroy((err) => {
+        if (err) {
+          console.error("Session destruction error:", err);
+          return res
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json({ error: "Failed to destroy session" });
+        }
+      });
+    }
+    return res
+      .status(HttpStatus.OK)
+      .json({ message: "Logged out successfully" });
   }
 );
