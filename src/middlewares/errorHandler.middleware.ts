3de5f962a721946de5132694b446cdf36dd4c5c9
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-02-21 15:51:01 +0530

    Add resigter api

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -1,6 +1,20 @@
+import { ErrorRequestHandler, Response } from "express";
+import { HttpStatus } from "../config/http.config";
 import { AppError } from "../utils/appError";
-import { HttpStatus } from "./../config/http.config";
-import { ErrorRequestHandler } from "express";
+import { z, ZodError } from "zod";
+import { ErrorCodeEnum } from "../enums/errorCode.enum";
+
+const formatZodError = (res: Response, error: z.ZodError) => {
+  const errors = error?.issues?.map((err) => ({
+    field: err.path.join("."),
+    message: err.message,
+  }));
+  return res.status(HttpStatus.BAD_REQUEST).json({
+    message: "Validation failed",
+    errors: errors,
+    errorCode: ErrorCodeEnum.VALIDATION_ERROR,
+  });
+};
 
 export const errorHandler: ErrorRequestHandler = (
   error,
@@ -8,12 +22,16 @@ export const errorHandler: ErrorRequestHandler = (
   res,
   next
 ): any => {
-  console.log(`Error Occurred on PATH: ${req.path}`, error);
+  console.error(`Error Occurred on PATH: ${req.path} `, error);
 
   if (error instanceof SyntaxError) {
     return res.status(HttpStatus.BAD_REQUEST).json({
-      message: "Invalid JSON format. Please check your request body",
+      message: "Invalid JSON format. Please check your request body.",
     });
+  }
+
+  if (error instanceof ZodError) {
+    return formatZodError(res, error);
   }
 
   if (error instanceof AppError) {
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,8 +4,9 @@ import AccountModel from "../models/account.model";
 import WorkspaceModel from "../models/workspace.model";
 import RoleModel from "../models/roles-permission.model";
 import { Roles } from "../enums/role.enum";
-import { NotFoundException } from "../utils/appError";
+import { BadRequestException, NotFoundException } from "../utils/appError";
 import MemberModel from "../models/member.model";
+import { ProviderEnum } from "../enums/account-provider.enum";
 
 export const loginOrCreateAccountService = async (data: {
   provider: string;
@@ -66,6 +67,69 @@ export const loginOrCreateAccountService = async (data: {
     }
 
     return { user };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const registerUserService = async (body: {
+  email: string;
+  name: string;
+  password: string;
+}) => {
+  const { email, name, password } = body;
+
+  try {
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestException("Email already exists");
+    }
+
+    const user = new UserModel({
+      email,
+      name,
+      password,
+    });
+    await user.save();
+
+    const account = new AccountModel({
+      userId: user._id,
+      provider: ProviderEnum.EMAIL,
+      providerId: email,
+    });
+    await account.save();
+
+    // 3. Create a new workspace for the new user
+    const workspace = new WorkspaceModel({
+      name: `My Workspace`,
+      description: `Workspace created for ${user.name}`,
+      owner: user._id,
+    });
+    await workspace.save();
+
+    const ownerRole = await RoleModel.findOne({
+      name: Roles.OWNER,
+    });
+
+    if (!ownerRole) {
+      throw new NotFoundException("Owner role not found");
+    }
+
+    const member = new MemberModel({
+      userId: user._id,
+      workspaceId: workspace._id,
+      role: ownerRole._id,
+      joinedAt: new Date(),
+    });
+    await member.save();
+
+    user.currentWorkspace = workspace._id as mongoose.Types.ObjectId;
+    await user.save();
+
+    return {
+      userId: user._id,
+      workspaceId: workspace._id,
+    };
   } catch (error) {
     throw error;
   }
